12747668bba48e9c470fabc8c4edc4e9d7f47e9c
Kshitij Kumar <kshitij2212@gmail.com>
2020-11-23 23:25:55 +0530

    Basic motion layout

--- app/src/main/java/com/example/navigationcomponent/GalleryFragment.kt
+++ app/src/main/java/com/example/navigationcomponent/GalleryFragment.kt
@@ -1,6 +1,19 @@
 package com.example.navigationcomponent
 
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.navigationcomponent.databinding.FragmentGalleryBinding
 
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+    private lateinit var viewBinding: FragmentGalleryBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding = FragmentGalleryBinding.bind(view)
+
+        viewBinding.tvArticle.movementMethod = ScrollingMovementMethod()
+    }
 }
