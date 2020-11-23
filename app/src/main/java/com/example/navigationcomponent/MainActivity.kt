b94a78e7a718aeca4ef4b79ac56d25bf31b91fcc
Kshitij Kumar <kshitij2212@gmail.com>
2020-11-23 19:46:30 +0530

    viewBinding implemented

--- app/src/main/java/com/example/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.navigationcomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.navigationcomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.tvHelloWorld.setOnClickListener {
+            Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
+        }
     }
 }
